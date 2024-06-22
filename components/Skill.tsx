3acec6b0d8bc61ab9f2aeb57408d4e0eb906e7d1
north-tower <muneshmyke@gmail.com>
2024-06-22 11:47:53 +0300

    6

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 type Props = {
   directionLeft?: boolean;
 }
-function Skill({ directionLeft, name }: Props){
+function Skill({ directionLeft, url, level }: Props){
   return(
     <div className='group relative flex cursor-pointer'>
        <motion.img 
@@ -13,11 +13,10 @@ function Skill({ directionLeft, name }: Props){
         }}
         transition={{duration: 1}}
         whileInView={{opacity: 1, x: 0}}
-        src="https://cdn.sanity.io/images/g86efv0h/production/5106dd90453602cc4f28088b03ff0b23d770563c-800x800.svg"
+        src={url}
         className='rounded-full border border-gray-500 object-cover w-16 h-16
         md:w-16 md:h-16 xl:w-16 xl:h-16 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
-      {name}
        <div className='absolute opacity-0 group-hover:opacity-80 transition
       }
       duration-300 ease-in-out group-hover:bg-white 
@@ -25,7 +24,7 @@ function Skill({ directionLeft, name }: Props){
       xl:w-16 xl:h-16 rounded-full z-0'>
         <div className='flex items-center justify-center h-full'>
           <p className='text-xl font-bold text-black opacity-100'>
-            100%
+            {level}
           </p>
         </div>
       </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,11 +4,30 @@ import { motion } from 'framer-motion'
 import Skill from './Skill'
 
 const skills = [
-    { name: 'SQL', level: 'Advanced' },
-    { name: 'PHP', level: 'Advanced' },
-    { name: 'HTML', level: 'Advanced' },
-    { name: 'CSS', level: 'Advanced' },
-    { name: 'JavaScript', level: 'Intermediate' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/next-js.svg', level: '95' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/typescript.svg', level: '90' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/tailwind-css-2.svg', level: '95' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/react-2.svg', level: '98' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/logo-javascript.svg', level: '90' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/python-5.svg', level: '90' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/xampp.svg', level: '85' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/laravel-2.svg', level: '70' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/mysql-3.svg', level: '70' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/r-lang.svg', level: '99' },
+
+    { url: 'https://cdn.worldvectorlogo.com/logos/css-3.svg', level: '95' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/firebase-2.svg', level: '95' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/react-native-1.svg', level: '90' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg', level: '95' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/sanity.svg', level: '80' },
+    { url: 'https://cdn.worldvectorlogo.com/logos/html-1.svg', level: '98' },
+
+
+
+
+
+
+
     // Add more skills as needed
 ];
 function Skills() {
@@ -32,7 +51,7 @@ function Skills() {
 
         <div className='grid grid-cols-4 gap-5 absolute top-48  '>
            {skills.map((skill, index) => (
-                <Skill key={index} name={skill.name} level={skill.level} />
+                <Skill key={index} url={skill.url} level={skill.level} />
             ))}
          
 
