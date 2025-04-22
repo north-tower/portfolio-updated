820e398d8b43ab5dc0da3a0cd27e205ea7ad4ff7
Miki <muneshmyke@gmail.com>
2025-04-22 09:57:05 +0300

    Update ExperienceCard.tsx

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -3,64 +3,42 @@ import { motion } from 'framer-motion';
 
 export default function ExperienceCard() {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[300px] md:w-[400px] xl:w-[500px] snap-center bg-[#5e5c5c] p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 overflow-hidden'>
-      <div className='relative h-16 w-16'>
-        <motion.img
-          initial={{ y: -100, opacity: 0 }}
-          transition={{ duration: 1.2 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          className='h-full w-full rounded-full object-cover object-center'
-          src="https://cdn.sanity.i/images/g86efv0h/production/7e272592098820aae11fa8d3924101bcd691a620-500x500.png?w=2000&fit=max&auto=format"
-          alt=''
-        />
-      </div>
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+      <motion.img
+        initial={{ y: -100, opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        src="/company-logo-kilimo.jpg"
+        alt="Kilimo Feeds Limited"
+      />
 
-      <div className='px-1 md:px-10 text-center md:text-left'>
-        <h4 className='font-light'>Software Engineer</h4>
-        <p className='font-bold text-xs mt-1'>Kilimo Feeds Limited</p>
-        <div className='flex justify-center md:justify-start space-x-2 my-2'>
-          <img 
-            className="h-10 w-10 rounded-full"
-            src="https://cdn.worldvectorlogo.com/logos/next-js.svg"
-            alt=''
-          />
-           <img 
-            className="h-10 w-10 rounded-full"
-            src="https://cdn.worldvectorlogo.com/logos/typescript.svg"
-            alt=''
-          />
-           <img 
-            className="h-10 w-10 rounded-full"
-            src="https://cdn.worldvectorlogo.com/logos/tailwind-css-2.svg"
-            alt=''
-          />
-           <img 
-            className="h-10 w-10 rounded-full"
-            src="https://cdn.worldvectorlogo.com/logos/react-2.svg"
-            alt=''
-          />
-            <img 
-            className="h-10 w-10 rounded-full"
-            src="https://cdn.worldvectorlogo.com/logos/logo-javascript.svg"
-            alt=''
-          />
+      <div className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">Software Engineer</h4>
+        <p className="font-bold text-2xl mt-1">Kilimo Feeds Limited</p>
+        <div className="flex space-x-2 my-2">
+          <img className="h-10 w-10 rounded-full" src="/tech-icon-1.png" alt="" />
+          <img className="h-10 w-10 rounded-full" src="/tech-icon-2.png" alt="" />
+          <img className="h-10 w-10 rounded-full" src="/tech-icon-3.png" alt="" />
+          <img className="h-10 w-10 rounded-full" src="/tech-icon-4.png" alt="" />
         </div>
-        <p className='uppercase py-1 text-gray-300 text-sm'>
-          JAN 2024 - Present
-        </p>
-        <ul className='list-disc space-y-4 ml-5 text-sm text-left h-24 overflow-y-scroll scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
+        <p className="uppercase py-5 text-gray-300">JAN 2024 - Present</p>
+
+        <ul className="list-disc space-y-4 ml-5 text-lg">
           <li>Design and Develop Software: Create, test, and maintain software applications.</li>
           <li>Code Writing: Write efficient, maintainable, and scalable code.</li>
           <li>Problem-Solving: Debug and resolve technical issues.</li>
           <li>Collaboration: Work with cross-functional teams including designers, product managers, and other engineers.</li>
           {/* Uncomment and add more list items as needed */}
-          {/* <li>System Architecture: Design and implement software architecture and infrastructure.</li>
+          {/* 
+          <li>System Architecture: Design and implement software architecture and infrastructure.</li>
           <li>Code Review: Review and provide feedback on code written by peers.</li>
           <li>Documentation: Create and maintain technical documentation.</li>
           <li>Testing: Conduct unit, integration, and system testing.</li>
           <li>Continuous Learning: Stay updated with the latest industry trends and technologies.</li>
-          <li>Client Interaction: Gather requirements and feedback from stakeholders and users.</li> */}
+          <li>Client Interaction: Gather requirements and feedback from stakeholders and users.</li>
+          */}
         </ul>
       </div>
     </article>
