d0d16b1b6234898ab79f5805f7467574e5b6a3ef
Miki <muneshmyke@gmail.com>
2025-04-22 09:56:32 +0300

    Update WorkExperience.tsx

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -5,33 +5,24 @@ import ExperienceCard from './ExperienceCard'
 import ExperienceCard2 from './ExperienceCard2'
 import ExperienceCard3 from './ExperienceCard3'
 
-
 function WorkExperience() {
   return (
-    <motion.div
-    initial={{opacity: 0}}
-    whileInView={{ opacity:1}}
-    transition={{ duration: 1.5}}
-     className='h-screen flex relative overflow-hidden flex-col
-    text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto 
-    items-center'>
-        <h3 className='absolute top-24 uppercase 
-        tracking-[20px] text-gray-500 text-2xl '>
-         Experience
-        </h3>
-        <div className='w-full flex space-x-5 overflow-x-scroll
-         p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
-          <ExperienceCard />
+    <motion.div 
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Experience
+      </h3>
 
-
-
-          <ExperienceCard2 />
-          <ExperienceCard3 />
-          
-
-
-        </div>
-        </motion.div>
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+        <ExperienceCard />
+        <ExperienceCard2 />
+        <ExperienceCard3 />
+      </div>
+    </motion.div>
   )
 }
 
