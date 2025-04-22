f776b577b7504aa5c004d04d4ca056701c2e0dc9
Miki <muneshmyke@gmail.com>
2025-04-22 10:04:57 +0300

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,38 +1,32 @@
-import { motion } from "framer-motion"
+import React from 'react';
+import { motion } from 'framer-motion';
 
-type Props = {
-  directionLeft?: boolean;
-  url?: string;
-  level?: string;
-}
-function Skill({ directionLeft, url, level }: Props){
-  return(
-    <div className='group relative flex cursor-pointer'>
-       <motion.img 
+function Skill({ url, level, directionLeft }) {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
         initial={{
-          x : directionLeft ? -200 : 200,
-          opacity : 0,
+          x: directionLeft ? -60 : 60,
+          opacity: 0
         }}
-        transition={{duration: 1}}
-        whileInView={{opacity: 1, x: 0}}
+        transition={{ duration: 0.8 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
         src={url}
-        className='rounded-full border border-gray-500 object-cover w-16 h-16
-        md:w-16 md:h-16 xl:w-16 xl:h-16 filter group-hover:grayscale transition duration-300 ease-in-out'
+        alt="Skill icon"
+        className="rounded-full border border-gray-500 object-contain w-16 h-16 md:w-20 md:h-20 lg:h-24 lg:w-24 
+        filter group-hover:grayscale transition duration-300 ease-in-out bg-white p-1"
       />
-       <div className='absolute opacity-0 group-hover:opacity-80 transition
-      }
-      duration-300 ease-in-out group-hover:bg-white 
-      h-16 w-16 md:w-16 md:h-16
-      xl:w-16 xl:h-16 rounded-full z-0'>
-        <div className='flex items-center justify-center h-full'>
-          <p className='text-xl font-bold text-black opacity-100'>
-            {level}
-          </p>
+      
+      <div className="absolute opacity-0 group-hover:opacity-90 transition duration-300 
+      ease-in-out group-hover:bg-white w-16 h-16 md:w-20 md:h-20 lg:h-24 lg:w-24 
+      rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-2xl font-bold text-black opacity-100">{level}%</p>
         </div>
       </div>
-    
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Skill
+export default Skill;
